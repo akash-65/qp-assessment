c648496bbbfde70b737fd305dd87200fa5d6a12c
akash-65 <akash.ap1283@gmail.com>
2025-02-14 22:46:26 +0530

    Initial Commit

---
+++ src/main/java/com/example/qp_assessment/QpAssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.qp_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpAssessmentApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QpAssessmentApplication.class, args);
+	}
+
+}
